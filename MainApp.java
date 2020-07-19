028b15d1174afbbbbb071ba42026ced5567c9d09
Rahuldixit165 <67890795+Rahuldixit165@users.noreply.github.com>
2020-07-20 00:50:48 +0530

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,11 @@
+package com.boraji.tutorial.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+   public static void main(String[] args) {
+      System.out.println("Hello World Welcome To Spring-Boot");
+   }
+}
